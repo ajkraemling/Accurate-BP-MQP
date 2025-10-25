1057bfe23a00c4a0abd2e8e564a5e9f42896033e
Harleen Kaur <courtnycatastrophe1997@gmail.com>
2025-10-25 13:57:21 -0400

    LED Blinks with ESP32!  :)

--- ESP32-V2/src/main.cpp
+++ ESP32-V2/src/main.cpp
@@ -5,13 +5,13 @@ int retVal = true;
 void setup() {
   // put your setup code here, to run once:
    Serial.begin(115200);
-   pinMode(15, OUTPUT);
+   pinMode(15, OUTPUT); //initalize LED with Pin Location
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(15, HIGH);
+  digitalWrite(15, HIGH); //LED turns on
   delay(100);
-  digitalWrite(15,LOW);
+  digitalWrite(15,LOW); //LED turns off
   delay(100);
 }
